7ca54336b92610889161647b6da06950b0574c5a
Matthieu Leclercq <matthieu-leclercq@hotmail.be>
2018-03-19 12:34:47 +0100

    Test de modif

--- app/src/main/java/com/example/matthieu/minipoll/ConnectionActivity.java
+++ app/src/main/java/com/example/matthieu/minipoll/ConnectionActivity.java
@@ -10,6 +10,6 @@ public class ConnectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
-        Toast.makeText(ConnectionActivity.this,"Hello Matthieu",Toast.LENGTH_SHORT);
+        Toast.makeText(ConnectionActivity.this,"Hello Julien",Toast.LENGTH_SHORT);
     }
 }
