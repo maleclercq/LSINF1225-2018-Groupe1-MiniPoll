9076ffe683d206d31c2cf881bf6e411ebc06fb8e
MoinnetA <adrien.moinnet@student.uclouvain.be>
2018-04-26 14:15:37 +0200

    addfriend

--- app/src/main/java/com/example/matthieu/minipoll/AddFriend.java
+++ app/src/main/java/com/example/matthieu/minipoll/AddFriend.java
@@ -1,13 +1,46 @@
 package com.example.matthieu.minipoll;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class AddFriend extends AppCompatActivity {
+
+    Utilisateur u;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_friend);
+        setContentView(R.layout.activity_friend);
+
+        Intent i = getIntent();
+        u= (Utilisateur)i.getSerializableExtra("utilisateur");
+
+        final DataBaseHelper myDbHelper = new DataBaseHelper(FriendActivity.this);
+        try {
+            myDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            Toast.makeText(FriendActivity.this, "Unable to create database", Toast.LENGTH_LONG).show();
+            throw new Error("Unable to create database");
+        }
+        try {
+            myDbHelper.openDataBase();
+        } catch (SQLException sqle) {
+            Toast.makeText(FriendActivity.this, "Unable to open database", Toast.LENGTH_LONG).show();
+            throw sqle;
+        }
+        //A ouvert la dataBase et la stocke dans myDbHelper
+
+
+        String [] whereArgs={u.pseudo};//les conditions de la requete sql
+        Cursor c=myDbHelper.rawQuery("select AMIS from AMIS where u=? ",whereArgs);//on fait la requete
+
+        String [][] tab=myDbHelper.createTabFromCursor(c,6);
+
     }
 }
