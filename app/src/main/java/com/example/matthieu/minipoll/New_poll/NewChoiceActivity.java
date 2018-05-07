12dac34e0af1bbc7fa9c7475158c91a07e6c3233
Célia FORET <celiaforet@wifi-student1-864.sri.ucl.ac.be>
2018-05-07 11:51:11 +0200

    Merge branch 'master' of /Users/celiaforet/StudioProjects/LSINF1225-2018-Groupe1-MiniPoll with conflicts.

--- app/src/main/java/com/example/matthieu/minipoll/New_poll/NewChoiceActivity.java
+++ app/src/main/java/com/example/matthieu/minipoll/New_poll/NewChoiceActivity.java
@@ -5,8 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteStatement;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
